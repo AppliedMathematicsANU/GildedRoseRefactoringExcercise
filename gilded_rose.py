7a3b806c013e4c1da1cbfb97ec45b93f135142b0
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2023-10-06 10:45:49 +1100

    first session result

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,39 +1,55 @@
 # -*- coding: utf-8 -*-
 
+from enum import Enum
+
+class Products(Enum):
+    BackstagePass = "Backstage passes to a TAFKAL80ETC concert"
+    Sulfuras = "Sulfuras, Hand of Ragnaros"
+    Brie = "Aged Brie"
+    Other = ""
+
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
+    def update_quality(self, max_quality=50, quality_cutoff_short=5, quality_cutoff_long=10):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name != Products.Brie.value and item.name != Products.BackstagePass.value:
+                if item.name != Products.Sulfuras.value:
+                    self.decrement_quality(item)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+                self.increment_quality(item, max_quality)
+                if item.quality < max_quality:
+                    if item.name == Products.BackstagePass.value:
+                        if item.sell_in < quality_cutoff_long + 1:
+                            self.increment_quality(item, max_quality)
+                        if item.sell_in < quality_cutoff_short + 1:
+                            self.increment_quality(item, max_quality)
+            if item.name != Products.Sulfuras.value:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                self.expired(max_quality, item)
+
+    def increment_quality(self, item, max_quality):
+        if item.quality < max_quality:
+            item.quality = item.quality + 1
+
+    def decrement_quality(self, item):
+        if item.quality > 0:
+            item.quality = item.quality - 1
+
+    def expired(self, max_quality, item):
+        if item.name != Products.Brie.value:
+            if item.name != Products.BackstagePass.value:
+                if item.quality > 0:
+                    if item.name != Products.Sulfuras.value:
+                        self.decrement_quality(item)
+            else:
+                item.quality = item.quality - item.quality
+        else:
+            self.increment_quality(item, max_quality)
 
 
 class Item:
