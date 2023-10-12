c6bcc268587aa37a6793fe6d7967bc70fef03c74
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2023-10-13 10:54:35 +1100

    second session result

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,6 +8,11 @@ class Products(Enum):
     Brie = "Aged Brie"
     Other = ""
 
+# no docstrings
+# still have hard-coded constants
+
+# deeply nested if statements
+# logic for different cases is intermingled
 
 class GildedRose(object):
 
@@ -16,9 +21,8 @@ class GildedRose(object):
 
     def update_quality(self, max_quality=50, quality_cutoff_short=5, quality_cutoff_long=10):
         for item in self.items:
-            if item.name != Products.Brie.value and item.name != Products.BackstagePass.value:
-                if item.name != Products.Sulfuras.value:
-                    self.decrement_quality(item)
+            if self.item_degrades(item):
+                self.decrement_quality(item)
             else:
                 self.increment_quality(item, max_quality)
                 if item.quality < max_quality:
@@ -27,29 +31,37 @@ class GildedRose(object):
                             self.increment_quality(item, max_quality)
                         if item.sell_in < quality_cutoff_short + 1:
                             self.increment_quality(item, max_quality)
+
             if item.name != Products.Sulfuras.value:
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
+
             if item.sell_in < 0:
-                self.expired(max_quality, item)
+                self.update_quality_when_expired(max_quality, item)
 
     def increment_quality(self, item, max_quality):
         if item.quality < max_quality:
-            item.quality = item.quality + 1
+            item.quality += 1
 
     def decrement_quality(self, item):
         if item.quality > 0:
-            item.quality = item.quality - 1
+            item.quality -= 1
 
-    def expired(self, max_quality, item):
-        if item.name != Products.Brie.value:
-            if item.name != Products.BackstagePass.value:
-                if item.quality > 0:
-                    if item.name != Products.Sulfuras.value:
-                        self.decrement_quality(item)
-            else:
-                item.quality = item.quality - item.quality
-        else:
+    def update_quality_when_expired(self, max_quality, item):
+        if item.name == Products.Sulfuras.value:
+            pass
+        elif item.name == Products.Brie.value:
             self.increment_quality(item, max_quality)
+        elif item.name == Products.BackstagePass.value:
+            item.quality = 0
+        else:
+            self.decrement_quality(item)
+
+    def item_degrades(self, item):
+        return item.name not in (
+            Products.Brie.value,
+            Products.BackstagePass.value,
+            Products.Sulfuras.value
+        )
 
 
 class Item:
